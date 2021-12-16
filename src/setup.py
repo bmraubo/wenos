0db80eb4d5686b765def7d7c068dd42a31fe5237
bmraubo <bmraubo@gmail.com>
2021-12-16 13:53:55 +0000

    setup creates required files

--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,12 @@ from os.path import join
 required_files = [join("src", "user_info.json"), join("src", "api_keys.json")]
 
 
+def create_required_files(files):
+    for file in files:
+        if not check_file(file):
+            create_file(file)
+
+
 def check_file(file_name):
     return exists(file_name)
 
--- tests/setup_tests.py
+++ tests/setup_tests.py
@@ -1,6 +1,7 @@
 import pytest
 from src import setup
 from os import remove
+from os.path import join
 
 
 def test_file_exists():
@@ -16,3 +17,15 @@ def test_file_create():
 
     assert setup.check_file(file_name) == True
     remove(file_name)
+
+
+def test_feature_create_files():
+    test_files = [join("test.json"), join("test2.json")]
+
+    setup.create_required_files(test_files)
+
+    assert setup.check_file("test.json")
+    assert setup.check_file("test2.json")
+
+    remove("test.json")
+    remove("test2.json")
