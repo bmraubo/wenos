9c4b23792d5ae9803972c47aa6d9f0ce0b22c75f
bmraubo <bmraubo@gmail.com>
2021-12-18 09:54:48 +0000

    some reorganisation

--- tests/data_gatherer_tests.py
+++ tests/data_gatherer_tests.py
@@ -1,11 +1,11 @@
 import pytest
-from src.data_gatherer.data_gatherer import DataGatherer
+from src.weather_service import DataGatherer
 
 
 def test_read_api_keys():
     test_file = "tests/test_api_key.json"
 
-    test_gatherer = DataGatherer(test_file)
+    test_gatherer = DataGatherer.DataGatherer(test_file)
 
     expected_api_keys = {
         "service": "a service",
---
+++ tests/open_weather_probe_tests.py
@@ -0,0 +1,1 @@
+from src.weather_service
