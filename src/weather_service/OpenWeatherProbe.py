2b982bf237eb3216a2a95b48f810c513865252c4
bmraubo <bmraubo@gmail.com>
2021-12-18 10:45:46 +0000

    added make request to open weather probe

--- src/weather_service/OpenWeatherProbe.py
+++ src/weather_service/OpenWeatherProbe.py
@@ -1,8 +1,16 @@
+import requests
+
+
 class Probe:
     request_url = ""
+    units = "metric"
+    response = {}
 
     def __init__(self, api_key, lat_lon):
         self.request_url = self.create_request_url(api_key, lat_lon)
 
     def create_request_url(self, api_key, lat_lon):
-        return f"https://api.openweathermap.org/data/2.5/onecall?lat={lat_lon[0]}&lon={lat_lon[1]}&appid={api_key}"
+        return f"https://api.openweathermap.org/data/2.5/onecall?lat={lat_lon[0]}&lon={lat_lon[1]}&exclude=minutely&appid={api_key}&units={self.units}"
+
+    def make_request(self):
+        return requests.post(self.request_url)
--- tests/open_weather_probe_tests.py
+++ tests/open_weather_probe_tests.py
@@ -1,4 +1,7 @@
 from src.weather_service import OpenWeatherProbe
+from src.weather_service import DataGatherer
+import requests
+import os.path
 
 
 def test_create_request_url():
@@ -7,8 +10,17 @@ def test_create_request_url():
 
     weather_probe = OpenWeatherProbe.Probe(api_key, lat_lon)
 
-    expected_url = (
-        "https://api.openweathermap.org/data/2.5/onecall?lat=0.02&lon=0.01&appid=aaaa"
-    )
+    expected_url = "https://api.openweathermap.org/data/2.5/onecall?lat=0.02&lon=0.01&exclude=minutely&appid=aaaa&units=metric"
 
     assert weather_probe.request_url == expected_url
+
+
+def test_make_request():
+    data_gatherer = DataGatherer.DataGatherer(os.path.join("src", "api_keys.json"))
+    api_key = data_gatherer.api_keys["open_weather"]["api_key"]
+    lat_lon = (51.026376, -1.315293)
+
+    probe = OpenWeatherProbe.Probe(api_key, lat_lon)
+    response = probe.make_request()
+
+    assert response.status_code == 200
