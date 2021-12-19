cf5d142995da96d006b8e560be066bbc7118cbf5
bmraubo <bmraubo@gmail.com>
2021-12-19 12:33:07 +0000

    OpenWeatherProbe returns weather data

--- src/weather_service/OpenWeatherProbe.py
+++ src/weather_service/OpenWeatherProbe.py
@@ -9,6 +9,10 @@ class Probe:
     def __init__(self, api_key, lat_lon):
         self.request_url = self.create_request_url(api_key, lat_lon)
 
+    def get_weather_data(self):
+        response = self.make_request()
+        return response.text
+
     def create_request_url(self, api_key, lat_lon):
         return f"https://api.openweathermap.org/data/2.5/onecall?lat={lat_lon[0]}&lon={lat_lon[1]}&exclude=minutely&appid={api_key}&units={self.units}"
 
