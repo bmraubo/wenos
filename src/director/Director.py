bab9f32b567506c134105a0fe1f8a2728a7b8ea5
bmraubo <bmraubo@gmail.com>
2021-12-22 12:38:14 +0000

    added is_it_notification_time to Director

--- src/director/Director.py
+++ src/director/Director.py
@@ -9,3 +9,7 @@ class Director:
 
     def add_notification_time(self, notification_time):
         self.notification_times.append(notification_time)
+
+    def is_it_notification_time(self):
+        current_time = self.check_time()
+        return current_time in self.notification_times
--- tests/director_tests.py
+++ tests/director_tests.py
@@ -17,3 +17,12 @@ def test_director_add_notification_time():
     director.add_notification_time(notification_time)
 
     assert director.notification_times[0] == notification_time
+
+
+def test_is_it_notification_time():
+    notification_time = datetime.now().strftime("%H:%M")
+
+    director = Director.Director()
+    director.add_notification_time(notification_time)
+
+    assert director.is_it_notification_time() == True
