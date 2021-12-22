4f035d63e18c0bfe6b5da6eb53e7419073bbe061
bmraubo <bmraubo@gmail.com>
2021-12-22 16:16:11 +0000

    extracted time managment to TimeManager

---
+++ src/director/TimeManager.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+
+class TimeManager:
+    notification_times = []
+
+    def check_time(self):
+        return datetime.now().strftime("%H:%M")
+
+    def add_notification_time(self, notification_time):
+        self.notification_times.append(notification_time)
+
+    def is_it_notification_time(self):
+        current_time = self.check_time()
+        return current_time in self.notification_times
+
+    def time_manager(self):
+        while True:
+            if self.is_it_notification_time():
+                # update the weather data
+                # send out an update email
+                pass
+            else:
+                pass
---
+++ tests/TimeManager_tests.py
@@ -0,0 +1,28 @@
+from datetime import datetime, time
+from src.director import TimeManager
+
+
+def test_time_check():
+    current_time = datetime.now().strftime("%H:%M")
+
+    time_manager = TimeManager.TimeManager()
+
+    assert time_manager.check_time() == current_time
+
+
+def test_add_notification_time():
+    notification_time = "01:01"
+
+    time_manager = TimeManager.TimeManager()
+    time_manager.add_notification_time(notification_time)
+
+    assert time_manager.notification_times[0] == notification_time
+
+
+def test_is_it_notification_time():
+    notification_time = datetime.now().strftime("%H:%M")
+
+    time_manager = TimeManager.TimeManager()
+    time_manager.add_notification_time(notification_time)
+
+    assert time_manager.is_it_notification_time() == True
