f0b6c388561d29830929cf253ed1790a734afef7
bmraubo <bmraubo@gmail.com>
2021-12-22 16:27:45 +0000

    added start method to director

--- src/director/Director.py
+++ src/director/Director.py
@@ -3,7 +3,19 @@ from src.director.TimeManager import TimeManager
 
 
 class Director:
-    time_manager = TimeManager()
+    def __init__(self):
+        self.time_manager = TimeManager()
 
     def add_notification_time(self, notification_time):
         self.time_manager.add_notification_time(notification_time)
+
+    def start(self):
+        while True:
+            if self.time_manager.is_it_notification_time():
+                # Update Weather Data
+                # Send weather data to Interpreter
+                # Send Data Packet to Composer
+                # Send Email
+                pass
+            else:
+                pass
