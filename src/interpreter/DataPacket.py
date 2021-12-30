61353b835fc5c9c12f4267192a6641ac2416c534
bmraubo <bmraubo@gmail.com>
2021-12-30 11:58:28 +0000

    data packet send current weather data to builder

--- src/interpreter/DataPacket.py
+++ src/interpreter/DataPacket.py
@@ -1,3 +1,10 @@
 class DataPacket:
     def __init__(self, data_packet_builder):
         self.builder = data_packet_builder
+
+    def add_current_weather(self, current_weather):
+        for key, value in current_weather.items():
+            self.builder.set_current_weather(key, value)
+
+    def generate_data_packet(self):
+        self.data = self.builder.generate_data_packet()
--- tests/DataPacket_tests.py
+++ tests/DataPacket_tests.py
@@ -8,3 +8,27 @@ def test_create_data_packet():
     data_packet = DataPacket(data_packet_builder)
 
     assert data_packet.builder != None
+
+
+def test_add_current_data():
+    current_data = {
+        "time": "10:00",
+        "sunrise": "09:00",
+        "sunset": "17:00",
+        "temp": 10,
+        "feelslike": 20,
+        "humidity": 10,
+        "uvi": 10,
+        "wind_speed": 0.9,
+        "wind_gusts": 1.2,
+        "weather": {"general": "raining", "description": "cats and dogs"},
+    }
+
+    data_packet_builder = DataPacketBuilder()
+    data_packet = DataPacket(data_packet_builder)
+
+    data_packet.add_current_weather(current_data)
+    data_packet.generate_data_packet()
+
+    assert data_packet.data["current"]["temp"] == 10
+    assert data_packet.data["current"] == current_data
