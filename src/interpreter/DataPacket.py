28891cf3f9a0e14033e7aa70246d975dcad7bb16
bmraubo <bmraubo@gmail.com>
2021-12-30 11:29:39 +0000

    added data packet class which takes a builder object

---
+++ src/interpreter/DataPacket.py
@@ -0,0 +1,3 @@
+class DataPacket:
+    def __init__(self, data_packet_builder):
+        self.builder = data_packet_builder
--- tests/DataPacket_tests.py
+++ tests/DataPacket_tests.py
@@ -1,1 +1,10 @@
 import pytest
+from src.interpreter.DataPacket import DataPacket
+from src.interpreter.DataPacketBuilder import DataPacketBuilder
+
+
+def test_create_data_packet():
+    data_packet_builder = DataPacketBuilder()
+    data_packet = DataPacket(data_packet_builder)
+
+    assert data_packet.builder != None
